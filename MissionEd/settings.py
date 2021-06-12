03eb0e1ec2ef530fa48c0c8274e614586b76d977
amarpsp10 <amarpsp10@gmail.com>
2021-06-12 06:46:35 -0700

    Debug set to ture

--- MissionEd/settings.py
+++ MissionEd/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%7*e6xfh=s%1n+#l6(sjf&*i6qkam^9@skad4mie^$h$7*=2xs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
